6192f0555f956b7b83af0b0c5e8a75604cee93a2
Christie Lynam <christielynam@gmail.com>
2018-08-27 12:27:23 -0600

    Add live code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import AddToDoForm from '../../containers/AddToDoForm'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello World</p>
+        <h1>My ToDo List</h1>
+        <AddToDoForm />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import rootReducer from './reducers';
 import './index.css';
 
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools)
+
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
